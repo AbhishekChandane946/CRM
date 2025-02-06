5ba97294c395e85f2fd0dd2726f74f1de62d9d4d
AbhishekChandane946 <abhishek.chandane@sec2payindia.com>
2025-02-06 19:12:13 +0530

    dd

--- header.php
+++ header.php
@@ -8,27 +8,36 @@
 * Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
 -->
 <html lang="en">
-  <head> 
-    <link rel="stylesheet" href="./dist/css/product_dashboard.css">  
-  
-      <!-- CSS files -->
-      <link href="./dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Card actions - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <!-- CSS files -->
+    <link href="./dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
     <link href="./dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
     <link href="./dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
     <link href="./dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet"/>
     <link href="./dist/css/demo.min.css?1692870487" rel="stylesheet"/>
-    <link rel="stylesheet" href="comman_styles.css">  
+
+    <link rel="stylesheet" href="comman_styles.css">   
      
-     <!-- Edited : Abhishek Chandane --> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Edited : Abhishek Chandane -->
-     
-    <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
+     <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
 
-
-
-
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
   </head>
+
+
+
+
   <body >
     <script src="./dist/js/demo-theme.min.js?1692870487"></script>
     <div class="page">
