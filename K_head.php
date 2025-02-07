aa752120470a074b2f6aacb11153e2ccc9515a62
AbhishekChandane946 <abhishek.chandane@sec2payindia.com>
2025-02-07 10:48:46 +0530

    dd

--- K_head.php
+++ K_head.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+
+    <link rel="stylesheet" type="text/css" href="./dist/css/dx.material.blue.light.css" />
+    <link rel="stylesheet" type="text/css" href="kanbanBoard.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="./dist/css/tabler.min.css?1692870487" rel="stylesheet" />
     <link href="./dist/css/tabler-flags.min.css?1692870487" rel="stylesheet" />
@@ -12,17 +15,17 @@
     <link href="./dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet" />
     <link href="./dist/css/demo.min.css?1692870487" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
     <link href=" https://cdn.jsdelivr.net/npm/sweetalert2@11.15.10/dist/sweetalert2.min.css " rel="stylesheet">
     <script>
         window.jQuery || document.write(decodeURIComponent('%3Cscript src="js/jquery.min.js"%3E%3C/script%3E'))
     </script>
-    <link rel="stylesheet" type="text/css" href="./dist/css/dx.material.blue.light.css" />
+   
     <script src="./dist/js/dx.all.js"></script>
     <script src="data.js"></script>
-    <link rel="stylesheet" type="text/css" href="kanbanBoard.css" />
+    
 </head>
 <style>
     ::ng-deep .dx-sortable {
--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <title> CRM</title>
     <!-- CSS files -->
     <link rel="stylesheet" href="dist/css/comman_styles.css"> 
-    
+
     <link href="./dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
     <link href="./dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
     <link href="./dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
